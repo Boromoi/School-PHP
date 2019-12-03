1c4f2ee9445b2d3eb4b26667073c1031ce742045
LiamSRoc <Lvanslingerlandt@roc-dev.com>
2019-12-03 14:19:18 +0100

    Klaar met h03.

--- h03/opdracht6/index.php
+++ h03/opdracht6/index.php
@@ -1,51 +1,45 @@
 <!DOCTYPE>
 <html>
 <head>
-
+    <style>
+        img {
+            width: 30px;
+            height: 30px;
+        }
+    </style>
 </head>
 <body>
 <?php
-$spartelkuikens = array([25]);
-$waterbuffels = array([32]);
-$plonsmderin = array([11]);
-$bommetje = array([23]);
+$spartelkuikens = 25;
+$waterbuffels = 32;
+$plonsmderin = 11;
+$bommetje = 23;
 
-foreach ($spartelkuikens as &$value) {
-    $value = $value / 5;
-}
+$spartelkuikens = $spartelkuikens / 5;
+$waterbuffels = $waterbuffels / 5;
+$plonsmderin = $plonsmderin / 5;
+$bommetje = $bommetje / 5;
 
 while ($spartelkuikens > 0.9) {
     echo "<img src='../img/zwem.png'>";
     $spartelkuikens--;
 }
 
-unset($value);
-
-foreach ($waterbuffels as &$value) {
-    $value = $value / 5;
-}
+echo "<br>";
 
 while ($waterbuffels > 0.9) {
     echo "<img src='../img/zwem.png'>";
     $waterbuffels--;
 }
 
-unset($value);
-
-foreach ($plonsmderin as &$value) {
-    $value = $value / 5;
-}
+echo "<br>";
 
 while ($plonsmderin > 0.9) {
     echo "<img src='../img/zwem.png'>";
     $plonsmderin--;
 }
 
-unset($value);
-
-foreach ($bommetje as &$value) {
-    $value = $value / 5;
-}
+echo "<br>";
 
 while ($bommetje > 0.9) {
     echo "<img src='../img/zwem.png'>";
---
+++ h03/opdracht7/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+<html>
+<head>
+
+</head>
+<body>
+<?php
+
+?>
+<ul>Welke php-code moet ingevuld worden op de plek van ..... => ..... ?</ul>
+<br>
+<ul>Er moet $value => $index</ul>
+</body>
+</html>
