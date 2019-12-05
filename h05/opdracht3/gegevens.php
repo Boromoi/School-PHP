84204891151436701ea6fbb7d005295b017ea1a0
LiamSRoc <Lvanslingerlandt@roc-dev.com>
2019-12-05 11:11:22 +0100

    .

--- h05/opdracht3/gegevens.php
+++ h05/opdracht3/gegevens.php
@@ -1,5 +1,7 @@
 <?php
 
+    $randomboolean = false;
+
 if ($_POST['wachtwoord'] == "") {
     echo "Je moet een wachtwoord invullen";
     echo "<a href=\"index.html\">Login</a>";
@@ -12,14 +14,22 @@ if ($_POST['email'] == "") {
 
 if ($_POST['wachtwoord'] == "doetje123" && $_POST['email'] == "piet@worldonline.nl") {
     header("Location: https://liamsroc.000webhostapp.com/h05/opdracht3/welkom.html"); /* Redirect browser */
+    $randomboolean == true;
     exit();
 } else if ($_POST['wachtwoord'] == "snoepje777" && $_POST['email'] == "klaas@carpets.nl") {
     header("Location: https://liamsroc.000webhostapp.com/h05/opdracht3/welkom.html"); /* Redirect browser */
+    $randomboolean == true;
     exit();
 } else if ($_POST['wachtwoord'] == "arkiearkie201" && $_POST['email'] == "truushendriks@wegweg.nl") {
     header("Location: https://liamsroc.000webhostapp.com/h05/opdracht3/welkom.html"); /* Redirect browser */
+    $randomboolean == true;
+    exit();
+} else if ($_POST['wachtwoord'] == "sans" && $_POST['email'] == "timmie.de.dikke.loser@gmail.com") {
+    header("Location: https://liamsroc.000webhostapp.com/h05/opdracht3/welkom.html"); /* Redirect browser */
+    $randomboolean == true;
     exit();
 } else {
     echo "Incorrecte inlog.";
     echo "<a href=\"index.html\">Login</a>";
+    $randomboolean == false;
 }
