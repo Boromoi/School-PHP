8efe2fdfcb20d818f16a1f34d105d84708826dab
LiamSRoc <Lvanslingerlandt@roc-dev.com>
2019-12-05 14:33:00 +0100

    .

--- h06/opdracht1/index.php
+++ h06/opdracht1/index.php
@@ -4,12 +4,7 @@ $connection = mysqli_connect("localhost", "Liam", "Sans","school","3308"); //The
 $query = "SELECT * FROM cursist"; //You don't need a ; like you do in SQL
 $result = mysqli_query($connection,'$query');
 
-echo "<table>"; // start a table tag in the HTML
-
-    while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-    echo "<tr><td>" . $row['cursistnr'] . "</td><td>" . $row['naam'] . "</td></tr>" . $row['roepnaam'] . "</td></tr>" . $row['straat'] . "</td></tr>" . $row['postcode'] . "</td></tr>" . $row['plaats'] . "</td></tr>" . $row['geslacht'] . "</td></tr>" . $row['geb_datum'] . "</td></tr>";  //$row['index'] the index here is a field name
-    }
-
-    echo "</table>"; //Close the table in HTML
+echo $result;
+mysqli_fetch_array($result);
 
 mysqli_close($connection); //Make sure to close out the database connection
