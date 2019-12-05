292f648139181ad0ff64ba090b5d9976771a5812
LiamSRoc <Lvanslingerlandt@roc-dev.com>
2019-12-05 10:59:26 +0100

    .

--- h05/opdracht3/gegevens.php
+++ h05/opdracht3/gegevens.php
@@ -1,21 +1,25 @@
 <?php
 
-if ($_POST['inlognaam'] == "") {
-    echo "Je moet een inlognaam invullen";
-    echo "<a href=\"form.html\">Formulier</a>";
-}
-
 if ($_POST['wachtwoord'] == "") {
     echo "Je moet een wachtwoord invullen";
-    echo "<a href=\"form.html\">Formulier</a>";
+    echo "<a href=\"index.html\">Login</a>";
 }
 
 if ($_POST['email'] == "") {
     echo "Je moet een email invullen";
-    echo "<a href=\"form.html\">Formulier</a>";
+    echo "<a href=\"index.html\">Login</a>";
 }
 
-if ($_POST['adres'] == "") {
-    echo "Je moet een adres invullen";
-    echo "<a href=\"form.html\">Formulier</a>";
+if ($_POST['wachtwoord'] == "doetje123" && $_POST['email'] == "piet@worldonline.nl") {
+    header("Location: https://liamsroc.000webhostapp.com/h05/opdracht3/welkom.html"); /* Redirect browser */
+    exit();
+} else if ($_POST['wachtwoord'] == "snoepje777" && $_POST['email'] == "klaas@carpets.nl") {
+    header("Location: https://liamsroc.000webhostapp.com/h05/opdracht3/welkom.html"); /* Redirect browser */
+    exit();
+} else if ($_POST['wachtwoord'] == "arkiearkie201" && $_POST['email'] == "truushendriks@wegweg.nl") {
+    header("Location: https://liamsroc.000webhostapp.com/h05/opdracht3/welkom.html"); /* Redirect browser */
+    exit();
+} else {
+    echo "Incorrecte inlog.";
+    echo "<a href=\"index.html\">Login</a>";
 }
