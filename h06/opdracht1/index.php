697b6036ceac9eac4b97056616562b95052c1c0f
LiamSRoc <Lvanslingerlandt@roc-dev.com>
2019-12-05 12:56:57 +0100

    .

--- h06/opdracht1/index.php
+++ h06/opdracht1/index.php
@@ -1,16 +1,15 @@
 <?php
-$connection = mysql_connect('localhost', 'root', ''); //The Blank string is the password
-mysql_select_db('hrmwaitrose');
+$connection = mysqli_connect('localhost', 'root', '','school','3308'); //The Blank string is the password
 
 $query = "SELECT * FROM cursist"; //You don't need a ; like you do in SQL
-$result = mysql_query($query);
+$result = mysqli_query($connection,'$query');
 
 echo "<table>"; // start a table tag in the HTML
 
-    while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
+    while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
     echo "<tr><td>" . $row['cursistnr'] . "</td><td>" . $row['naam'] . "</td></tr>" . $row['roepnaam'] . "</td></tr>" . $row['straat'] . "</td></tr>" . $row['postcode'] . "</td></tr>" . $row['plaats'] . "</td></tr>" . $row['geslacht'] . "</td></tr>" . $row['geb_datum'] . "</td></tr>";  //$row['index'] the index here is a field name
     }
 
     echo "</table>"; //Close the table in HTML
 
-mysql_close(); //Make sure to close out the database connection
+mysqli_close($connection); //Make sure to close out the database connection
