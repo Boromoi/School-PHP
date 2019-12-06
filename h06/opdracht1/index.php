3b99cb01686b899cdf0bd8e5f56015081fd11532
LiamSRoc <Lvanslingerlandt@roc-dev.com>
2019-12-06 11:05:05 +0100

    .

--- h06/opdracht1/index.php
+++ h06/opdracht1/index.php
@@ -1,10 +1,19 @@
 <?php
-$connection = mysqli_connect("localhost", "Liam", "Sans","school","3308"); //The Blank string is the password
+$conn = mysqli_connect("localhost", "Liam", "Sans","school","3308"); //The Blank string is the password
+if ($conn -> connect_error) {
+    die("Connection failed:". $conn->connect_error);
+}
 
-$query = "SELECT * FROM cursist"; //You don't need a ; like you do in SQL
-$result = mysqli_query($connection,'$query');
+$sql = "SELECT cursistnr, naam, roepnaam, straat, postcode, plaats, geslacht, geb_datum from cursist";
+$result = $conn-> query($sql);
 
-echo $result;
-mysqli_fetch_array($result);
+if ($result-> num_rows > 0) {
+    while ($row = $result-> fetch_assoc()) {
+        echo "<tr><td>". $row["cursistnr"]."</td><br><td>". $row["naam"]."</td><br><td>".$row["roepnaam"]."</td><br><td>".$row["straat"]."</td><br><td>".$row["postcode"]."</td><br><td>".$row["plaats"]."</td><br><td>".$row["geslacht"]."</td><br><td>".$row["geb_datum"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 result";
+}
 
-mysqli_close($connection); //Make sure to close out the database connection
+$conn -> close();
