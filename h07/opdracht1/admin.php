f56460aa70c3ad3b325f1cd411e28e407fa8a984
LiamSRoc <Lvanslingerlandt@roc-dev.com>
2019-12-18 14:07:01 +0100

    .

--- h07/opdracht1/admin.php
+++ h07/opdracht1/admin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (isset($_SESSION["user"] && $_SESSION["user"]["rol"] == "Administrator")) {
+if (isset($_SESSION["user"]) && $_SESSION["user"]["rol"] == "Administrator") {
     echo "<h1>Welkom ".$_SESSION["user"]["naam"]." op het admin gedeelte van de website</h1>";
     echo "<p><a href='login.php'>Login</a></p>";
 } else {
